1cdaaf5faac2ba03ba28f5e6f26a66dd2de93a34
hachazo <87842940+hachazo@users.noreply.github.com>
2023-04-08 19:54:09 -0300

    Add files via upload

---
+++ pila.adb
@@ -0,0 +1,61 @@
+package body Pila is
+
+   --Limpiar pila
+   procedure Clear(Pila: in out Tipopila) is
+   begin
+      Pila.Cabeza:=0;
+   end Clear;
+   
+   --Insertar elemento
+   procedure Push (Item: in tipodato; pila: in out tipopila) is
+   
+   begin
+      
+      if not Is_Full(pila) then --cheqea que la pila no este llena
+         pila.cabeza := pila.cabeza + 1;
+         pila.elemento(pila.cabeza) := Item;
+      end if;
+   
+   end Push;
+   
+   --Elimina elimina y devuelve el elemento en la cima de la pila
+   procedure Pop(Pila: in out Tipopila; Elemento: out Tipodato) is
+      
+      begin
+      
+         if not Is_Empty(Pila) then --chequea que la pila no este vacia
+            elemento:=pila.elemento(pila.cabeza);
+            Pila.Cabeza:=Pila.Cabeza -1;
+            end if;
+            
+      end pop;
+   
+      --devuelve el elemento en la cima de la pila sin eliminarlo
+      function Top(Pila: in Tipopila) return Tipodato is
+      
+      begin
+         
+         return pila.elemento(pila.cabeza);
+      
+      end Top;
+      
+      --devuelve true si la pila esta vacia
+      Function Is_Empty (Pila: in Tipopila) return Boolean is
+   
+   begin
+      
+      return Pila.Cabeza=0;
+      
+   end Is_Empty;
+   
+   --devuelve true si la pila esta llena
+   function Is_Full (Pila: in Tipopila) return Boolean is
+   
+   begin
+      
+      return Pila.Cabeza=Max;
+      
+   end Is_Full;
+      
+end Pila;
+
---
+++ pila.ads
@@ -0,0 +1,40 @@
+generic
+   
+   type Tipodato is private;
+   Max: positive; --tope de la pila
+
+package Pila is
+      
+      type Tipopila (Max: Positive) is limited private;
+      
+   procedure clear(pila: in out Tipopila);
+   --Elimina todos los elementos de la pila
+      
+   procedure Push (Item : in tipodato; pila: in out tipopila);
+   -- Inserta un elemento en la pila
+
+   procedure Pop (pila: in out tipopila; elemento: out tipodato);
+   -- Elimina y devuelve el elemento en la cima de la pila
+
+   function Top (pila: in tipopila) return tipodato;
+   -- Devuelve el elemento en la cima de la pila sin eliminarlo
+
+   function Is_Empty (pila: in tipopila) return Boolean;
+   -- Devuelve True si la pila está vacía
+
+   function Is_Full (pila: in tipopila) return Boolean;
+   -- Devuelve True si la pila está llena
+    
+   private
+   
+   type Arreglopila is array(Positive range <>) of Tipodato;
+   type Tipopila(Max:Positive) is
+      
+   record
+      
+   Cabeza:Natural:=0;
+   Elemento: Arreglopila(1..Max);
+      
+   end record;
+      
+end pila;
